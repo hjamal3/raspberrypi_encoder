52223159da1fa12c3538e4994975e2ac75674b88
hjamal <hjamal@andrew.cmu.edu>
2022-02-13 17:16:57 +0300

    Some comments and fixes

--- RPIEncoder.py
+++ RPIEncoder.py
@@ -2,6 +2,7 @@ import evdev
 import select
 import threading
 from timeit import default_timer as timer
+import time
 
 # an encoder class that returns the number of ticks and optionally revolutions
 # pin_a: gpio pin channel A of encoder is connected to
@@ -45,15 +46,20 @@ class Encoder:
 # ex:
 # pin_a = 5
 # ticks_per_rev = 600
-# start_time = 0
+# start_time = 0 # seconds
 # e = EncoderStamped(pin_a, ticks_per_rev, start_time)
 # (t, ticks) = e.getTicksStamped()
+# (t_latest_tick, ticks) = e.getTicksStampedAtEvent()
 class EncoderStamped:
-  def __init__(self, pin_a, ticks_per_rev = 1, start_time=0):
+  def __init__(self, pin_a, ticks_per_rev = 1, start_offset=0):
     self.ticks_per_rev = ticks_per_rev
     self.ticks = 0
-    self.start_time = start_time
-    self.start = timer()
+    self.start_time = start_offset # offset from 0 seconds
+    
+    self.start = timer() # used to measure time to getter
+    self.start_epoch = time.time() # used to measure time to event itself
+    self.event_time = self.start_epoch
+  
     self.device = evdev.InputDevice('/dev/input/by-path/platform-rotary@'
                                + str(hex(pin_a)[2:]) + '-event')
     self.encoder_exec_thread = threading.Thread(target=self.exec, name="encoder_exec_thread")
@@ -65,6 +71,7 @@ class EncoderStamped:
     for event in self.device.read_loop():
         if event.type == evdev.ecodes.EV_REL:
             self.ticks = self.ticks + event.value
+            self.event_time = event.timestamp()
 
   def getRevs(self):
     return self.ticks/self.ticks_per_rev
@@ -72,11 +79,21 @@ class EncoderStamped:
   def getTicks(self):
     return self.ticks
 
+  # returns time in seconds from constructor till now and total revolutions 
   def getRevsStamped(self):
     return (self.start_time+timer()-self.start, self.ticks/self.ticks_per_rev)
 
+  # returns time in seconds from constructor till now and total ticks 
   def getTicksStamped(self):
     return (self.start_time+timer()-self.start, self.ticks)
+
+  # returns time in seconds from constructor till last event (interrupt) and total revolutions 
+  def getRevsStampedAtEvent(self):
+    return (self.start_time+(self.event_time-self.start_epoch), self.ticks/self.ticks_per_rev)
+
+  # returns time in seconds from constructor till last event (interrupt) and total ticks 
+  def getTicksStampedAtEvent(self):
+    return (self.start_time+timer()+(self.event_time-self.start_epoch), self.ticks)
 
   def __del__(self):
     # body of destructor
